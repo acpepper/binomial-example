54fdf4f8671a9968f4eaa6a576b8d5f137e60ca2
Andrew Pepper <andrewpepper96@gmail.com>
2022-02-01 08:11:54 -0800

    feat: example invocations

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 export const binomial = (a: number, b: number, coeffs: Array<number>) => {
   return (coeffs[0] * a + coeffs[1]) * (coeffs[2] * b + coeffs[3]);
 };
+
+export const one = (a: number, b: number) => {
+  binomial(a, b, [-1, 2, 1, 1]);
+};
+
+export const two = (a: number, b: number) => {
+  binomial(a, b, [1, -1, 1, 2]);
+};
